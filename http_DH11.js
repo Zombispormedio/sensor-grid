f861afc60a94f8b0ecaa2a04ac435910ee6b3ff4
Xavier Serrano <zombispormedio007@gmail.com>
2016-06-01 17:27:18 +0200

    dh11.js

---
+++ http_DH11.js
@@ -0,0 +1,43 @@
+var sensorLib = require('node-dht-sensor');
+var env = require("./env");
+var unirest = require("unirest");
+
+var headers={
+    'Accept': 'application/json', 
+    'Content-Type': 'application/json',
+    "ClientID":env.credentials2.client_id,
+    "ClientSecret":env.credentials2.client_secret
+}
+
+var send=function(obj){
+    
+    unirest.post(env.PUSH_HOST+"/sensor_grid")
+    .headers(headers)
+    .send(obj)
+    .end();
+    
+}
+
+
+
+setInterval(function(){
+    var b = sensorLib.readSpec(11, 4);
+    var temperature=b.temperature;
+    var humidity=b.humidity;
+    console.log("Value at " + (new Date()) + ": " +temperature+ "C, " + humidity + "%");
+    var obj = {
+        data: [{
+            node_id: env.sensors2.temperature,
+            value: temperature
+        },
+        {
+            node_id:env.sensors2.humidity,
+            value:humidity
+        }]
+    };
+
+
+    send(obj);
+    
+    
+}, 5000);
---
+++ mqtt.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+nohup node mqtt_main.js > /dev/null 2>1 &
