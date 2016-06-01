06f30cfccc4e7a8963da4a48b524251b3bb25dd7
Xavier Serrano <zombispormedio007@gmail.com>
2016-06-01 18:21:42 +0200

    try catch

--- http_DHT11.js
+++ http_DHT11.js
@@ -2,42 +2,46 @@ var sensorLib = require('node-dht-sensor');
 var env = require("./env");
 var unirest = require("unirest");
 
-var headers={
-    'Accept': 'application/json', 
+var headers = {
+    'Accept': 'application/json',
     'Content-Type': 'application/json',
-    "ClientID":env.credentials2.client_id,
-    "ClientSecret":env.credentials2.client_secret
+    "ClientID": env.credentials2.client_id,
+    "ClientSecret": env.credentials2.client_secret
 }
 
-var send=function(obj){
-    
-    unirest.post(env.PUSH_HOST+"/sensor_grid")
-    .headers(headers)
-    .send(obj)
-    .end();
-    
+var send = function (obj) {
+
+    unirest.post(env.PUSH_HOST + "/sensor_grid")
+        .headers(headers)
+        .send(obj)
+        .end();
+
 }
 
 
+try {
+    setInterval(function () {
 
-setInterval(function(){
-    var b = sensorLib.readSpec(11, 4);
-    var temperature=b.temperature;
-    var humidity=b.humidity;
-    console.log("Value at " + (new Date()) + ": " +temperature+ "C, " + humidity + "%");
-    var obj = {
-        data: [{
-            node_id: env.sensors2.temperature,
-            value: temperature+""
-        },
-        {
-            node_id:env.sensors2.humidity,
-            value:humidity+""
-        }]
-    };
+        var b = sensorLib.readSpec(11, 4);
+        var temperature = b.temperature;
+        var humidity = b.humidity;
+        console.log("Value at " + (new Date()) + ": " + temperature + "C, " + humidity + "%");
+        var obj = {
+            data: [{
+                node_id: env.sensors2.temperature,
+                value: temperature + ""
+            },
+                {
+                    node_id: env.sensors2.humidity,
+                    value: humidity + ""
+                }]
+        };
 
 
-    send(obj);
-    
-    
-}, 5000);
+        send(obj);
+
+
+    }, 5000);
+} catch (err) {
+    console.log(err);
+}
